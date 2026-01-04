566fc1fcfa5090c1b0c1639e0b02a16af6e1ce7c
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-04 04:48:41 +0100

    learned about javascript promises

---
+++ JavaScript/Promises.js
@@ -0,0 +1,10 @@
+let stocks = {
+  Fruits: ["Strawberry", "Grapes", "Banana", "Apple"],
+  liquid: ["Water", "Ice"],
+  holder: ["Cone", "Cup", "Stick"],
+  toppings: ["Chocolate", "Peanut"],
+};
+
+let isShopOpen = true;
+
+let order = (time, work) => {};
