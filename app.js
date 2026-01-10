b1beb7bf9a6cfc761d80e3923b550e5f6886cbd5
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-11 00:41:01 +0100

    Continued learning about package.json

--- app.js
+++ app.js
@@ -12,3 +12,8 @@
 // manual approach (create package.json in the root, create properties e.t.c)
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
+
+const lodash = require("lodash");
+
+const items = [1, [2, [3, 4]]];
+const newItems = lodash.flatMapDeep(items);
