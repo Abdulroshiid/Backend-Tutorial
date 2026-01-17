8deadb32530db3a02610ff10500543e180f8e183
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-17 22:43:35 +0100

    Kept learning

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ console.log(newItems);
 const greetUser = `Hello, Abdulrasheed! Be proud of yourself. You've crossed another milestone`;
 console.log(greetUser);
 
-console.log(`And with time, everything changes.`);
+console.log(`And with time, everything changes. Though, it is tough`);
