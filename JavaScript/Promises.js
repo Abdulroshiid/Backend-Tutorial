f84d7da3567a384bc2f0cc3f9809777ac6a01736
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-17 23:43:21 +0100

    Kept learning

--- JavaScript/Promises.js
+++ JavaScript/Promises.js
@@ -1,30 +1,36 @@
-const { reject } = require("lodash");
+// const { reject } = require("lodash");
 
-function doSomething() {
-  return new Promise((resolve, reject) => {
-    const checkScenario = true;
-    if (checkScenario) {
-      resolve(`It went well`);
+// function doSomething() {
+//   return new Promise((resolve, reject) => {
+//     const checkScenario = true;
+//     if (checkScenario) {
+//       resolve(`It went well`);
+//     } else {
+//       reject(`Opps! It went totally wrong.`);
+//     }
+//   });
+// }
+// console.log(doSomething());
+
+const guessNumberPromise = new Promise((resolve, reject) => {
+  const randomNumber = Math.floor(Math.random() * 10);
+  console.log(randomNumber);
+
+  setTimeout(() => {
+    if (randomNumber > 5) {
+      resolve("Nice One!");
     } else {
-      reject(`Opps! It went totally wrong.`);
+      reject("Opps! Keep guessing man!");
     }
+  }, 4000);
+});
+
+console.log(guessNumberPromise);
+
+guessNumberPromise
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
   });
-}
-console.log(doSomething());
-
-function guessNumberPromise() {
-  return new Promise((resolve, reject) => {
-    const randomNumber = Math.floor(Math.random() * 10);
-    console.log(randomNumber);
-
-    setTimeout(() => {
-      if (randomNumber < 10) {
-        resolve("You guessed number 5 correctly and its' the right number!");
-      } else {
-        reject("Opps! Keep guessing man!");
-      }
-    }, 4000);
-  });
-}
-
-console.log(guessNumberPromise());
