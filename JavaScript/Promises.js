f607fbd2cc27cacefb02b5127e1c33d9b5bc6adf
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-17 23:13:26 +0100

    Kept learning

--- JavaScript/Promises.js
+++ JavaScript/Promises.js
@@ -1,59 +1,10 @@
-let stocks = {
-  Fruits: ["Strawberry", "Grapes", "Banana", "Apple"],
-  liquid: ["Water", "Ice"],
-  holder: ["Cone", "Cup", "Stick"],
-  toppings: ["Chocolate", "Peanut"],
+const promise = (resolve, reject) => {
+  const checkScenario = true;
+  if (checkScenario) {
+    resolve(`It went well`);
+  } else {
+    reject(`Opps! It went totally wrong.`);
+  }
 };
 
-let isShopOpen = false;
-
-let order = (time, work) => {
-  return new Promise((resolve, reject) => {
-    if (isShopOpen) {
-      setTimeout(() => {
-        resolve(work());
-      }, time);
-    } else {
-      reject(console.log(`Sorry, our shop is closed for the day!`));
-    }
-  });
-};
-
-order(2000, () => {
-  console.log(`${stocks.Fruits[3]} ice-cream order was placed`);
-})
-  .then(() => {
-    return order(2000, () => {
-      console.log(`The ${stocks.Fruits[3]} was cut`);
-    });
-  })
-  .then(() => {
-    return order(1000, () => {
-      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-    });
-  })
-  .then(() => {
-    return order(2000, () => {
-      console.log(`Start the machine`);
-    });
-  })
-  .then(() => {
-    return order(2000, () => {
-      console.log(`The customer selected ${stocks.holder[1]} as the holder`);
-    });
-  })
-  .then(() => {
-    return order(3000, () => {
-      console.log(
-        `The customer selected ${stocks.toppings[0]} for the topping`
-      );
-    });
-  })
-  .then(() => {
-    return order(3000, () => {
-      console.log(`${stocks.Fruits[3]} ice-cream was served`);
-    });
-  })
-  .catch(() => {
-    console.log(`Customer left`);
-  });
+console.log(promise);
