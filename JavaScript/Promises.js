7ff7402599af5c254a380fc11db6cf22c06343b0
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-01-18 22:52:03 +0100

    Kept learning

--- JavaScript/Promises.js
+++ JavaScript/Promises.js
@@ -1,6 +1,6 @@
 // const { reject } = require("lodash");
 
-const { reject } = require("lodash");
+const { reject, set, values } = require("lodash");
 
 // function doSomething() {
 //   return new Promise((resolve, reject) => {
@@ -39,41 +39,75 @@ const { reject } = require("lodash");
 
 // Make Eba
 
-const boilWater = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const isWaterHot = true;
-      if (isWaterHot) {
-        resolve(`Get ready to pour the G4!`);
-      } else {
-        reject(`Water is not yet hot to pour G4!, try reboiling again`);
-      }
-    }, 5000);
-  });
-};
+const boilWater = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const isWaterHot = true;
+    if (isWaterHot) {
+      resolve(`Get ready to pour the G4!`);
+    } else {
+      reject(`Water is not yet hot to pour G4!, try reboiling again`);
+    }
+  }, 5000);
+});
 
-const readyToPourG4 = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const pourG4 = true;
-      if (pourG4) {
-        resolve(`Pour the G4 into the boiling water`);
-      } else {
-        reject(`There is no G4 left in the store`);
-      }
-    }, 5000);
-  });
-};
+const readyToPourG4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const pourG4 = true;
+    if (pourG4) {
+      resolve(`Pour the G4 into the boiling water`);
+    } else {
+      reject(`There is no G4 left in the store`);
+    }
+  }, 1000);
+});
 
-const stiringProccess = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const stirG4 = true;
-      if (stirG4) {
-        resolve(`Stir the Eba `);
-      } else {
-        reject(`There is no turning stick available.`);
-      }
-    }, 5000);
+const stiringProccess = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const stirG4 = true;
+    if (stirG4) {
+      resolve(`Stir the Eba `);
+    } else {
+      reject(`There is no turning stick available.`);
+    }
+  }, 3000);
+});
+
+const makeSoup = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const isSoupReady = true;
+    if (isSoupReady) {
+      resolve(`Serve yourself and chow`);
+    } else {
+      reject(`Ewedu have finish.`);
+    }
+  }, 4000);
+});
+
+boilWater
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
   });
-};
+readyToPourG4
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+stiringProccess
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+makeSoup
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
