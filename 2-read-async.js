51da24c672323fde0fedf65b270416a8e47065a5
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-02-27 11:04:21 +0100

    An easier way to read and write async file using async/await

---
+++ 2-read-async.js
@@ -0,0 +1,27 @@
+// TARGETING THE IN-BUILT FILE SYSTEM MODEL (FS-MODEL)
+
+const { readFile, writeFile } = require("fs").promises;
+
+//CREATING AN ASYNC/AWAIT PROMISE
+
+const readAndWriteFile = async (firstPath, secondPath) => {
+  try {
+    // Read two files first, wrote one and read it again...
+    const readFirstFile = await readFile(firstPath, "utf-8");
+    console.log(readFirstFile);
+
+    const readSecondFile = await readFile(secondPath, "utf-8");
+    console.log(readSecondFile);
+
+    // Wrote the file and read it here...
+    await writeFile(
+      "./content/sub-folder/dashboard.txt",
+      `Testing if this could work... Then to the highest :)`,
+      "utf-8",
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readAndWriteFile("./folder/first.txt", "./folder/second.txt");
