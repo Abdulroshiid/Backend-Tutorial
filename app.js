8cded9be99b2029a2d955fa936ab18440200ca5c
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-02-27 12:40:41 +0100

    Started learning about event programming

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const EventEmitter = require("events");
 
 const customEmitter = new EventEmitter();
 
+customEmitter.on("response", (name, id) => {
+  console.log(`User ${name} with an id:${id}, received a response`);
+});
 customEmitter.on("response", () => {
-  console.log("Response received!");
+  console.log(`Some other logic goes here!`);
 });
 
-customEmitter.emit("response");
+customEmitter.emit("response", "Roshid", 73);
