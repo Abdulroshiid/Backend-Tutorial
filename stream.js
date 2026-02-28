8db28c269c42f839b935ad2534f685584034c4a3
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-02-28 06:41:14 +0100

    Learing streams

--- stream.js
+++ stream.js
@@ -1,7 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const { error } = require("console");
 const PORT = 5000;
 
-const server = http.createServer();
+const server = http.createServer(function (req, res) {
+  const fileStream = fs.createReadStream(
+    "./folder/sub-folder/dashboard.txt",
+    "utf-8",
+  );
+  fileStream.on("open", () => {
+    fileStream.pipe(res);
+  });
+  fileStream.on("error", (err) => {
+    res.end(err);
+  });
+});
 
 server.listen(PORT);
