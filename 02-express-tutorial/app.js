bce3ca16532fc715a3ce5fe1bac763727ef1dfdc
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-01 06:05:18 +0100

    Getting interesting

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,24 @@ const http = require("http");
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  console.log(`User hit the server!`);
-  res.end(`Home Page`);
+  const url = req.url;
+  // Home Page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write;
+    ("This is the Home Page");
+    res.end();
+    // About Page
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("This is the About Page");
+    res.end();
+    // Error Page
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("Resource not Found!!");
+    res.end();
+  }
 });
 
 server.listen(PORT);
