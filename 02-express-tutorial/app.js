a86a9d786a91b0363aace5430015d2a60612e660
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-01 14:32:25 +0100

    Updated

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -19,10 +19,20 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("This is the About Page");
     res.end();
-    // Error Page
+    // Styles Page
   } else if (url === "/styles") {
     res.writeHead(200, { "content-type": "text/css" });
     res.write(homeStyle);
+    res.end();
+    // Logo Page
+  } else if (url === "/logo") {
+    res.writeHead(200, { "content-type": "text/svg" });
+    res.write(homeLogo);
+    res.end();
+    // Logic Page (JavaScript)
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(`Page Not Found!`);
     res.end();
   }
 });
