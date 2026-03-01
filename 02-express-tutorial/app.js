665f2a9f10b6a68f4be4639d27c0f18f972abff1
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-01 02:06:58 +0100

    Continued learning express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require("http");
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+  console.log(`User hit the server!`);
+  res.end(`Home Page`);
+});
+
+server.listen(PORT);
