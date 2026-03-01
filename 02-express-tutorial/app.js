bd64921c80192e2955c6c5b8441086b0bbeca0ec
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-01 23:04:10 +0100

    Rounded up for the day. Learned to set up express btw

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+  res.status(200).send(`Home Page`);
+});
+app.get("/about", (req, res) => {
+  res.status(200).send(`This is the About Page`);
+});
+app.all("*", (req, res) => {
+  res.status(404).send(`Resource Not Found`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
