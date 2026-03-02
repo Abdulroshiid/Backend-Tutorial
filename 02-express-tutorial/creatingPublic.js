fbc83ddf4b76d75fdcc94325179d9410263a4969
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-02 23:10:39 +0100

    Understood how the public folder works as a static

---
+++ 02-express-tutorial/creatingPublic.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 5000;
+
+app.use(express.static("./public"));
+
+// Setup static and middleware
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send(`Page Not Found!`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Listening on port ${PORT}...`);
+});
