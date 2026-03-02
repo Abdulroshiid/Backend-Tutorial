4104d871c1d0f5a34e016630e58ff25c0e804696
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-02 14:01:56 +0100

    Got to understand the meaning of 304 status code

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,16 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.status(200).send(`Home Page`);
-});
-app.get("/about", (req, res) => {
-  res.status(200).send(`This is the About Page`);
-});
-app.all("*", (req, res) => {
-  res.status(404).send(`Resource Not Found`);
+app.get("/", (req, res) => {});
+
+app.get("*", (req, res) => {
+  res.status(404).send(`Page Not Found!`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is Listening on port ${PORT}...`);
 });
