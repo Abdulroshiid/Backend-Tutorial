b131de037fe9b2d4ac3ce7d578fdfa95b17ed936
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-02 17:10:13 +0100

    Learning about SSR

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
 app.get("*", (req, res) => {
   res.status(404).send(`Page Not Found!`);
