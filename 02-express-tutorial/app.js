4f176b4cccbb1603ab1f1e6a1874e1a0bc208a8a
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-02 22:37:26 +0100

    Created my first public folder btw

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,14 +3,10 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-app.get("/styles.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "./navbar-app/styles.css"));
-});
-app.get("/browser-app.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "./navbar-app/browser-app.js"));
 });
 
 app.get("*", (req, res) => {
