264e637d992e6a173418d41731f9510737224eb1
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-04 16:25:08 +0100

    Learnt to use the map method to loop through and select specific info that we would need by destructuring

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,11 +4,15 @@ const { products } = require("./data");
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1><a href="/products">Our Products</a>`);
+  res.send(`<h1>Home Page</h1><a href="/api/products">Our Products</a>`);
 });
 
-app.get("/products", (req, res) => {
-  res.json(products);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
 
 app.listen(PORT, () => {
