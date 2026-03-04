ae8e72928887ff350c16beb709a3b4cc8931d4ad
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-04 15:37:13 +0100

    Continued learning response with apis

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send(`<h1>Home Page</h1><a href="/api/products">Our Products</a>`);
 });
 
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
 app.listen(PORT, () => {
   console.log(`Server Listening on Port ${PORT}`);
 });
