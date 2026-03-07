667facb2c712143d38b8ba2efccc4d6df8e9c6b1
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-07 14:26:55 +0100

    Continued  learning about middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const logger = require("./logger");
 
 // req => middleware => res
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
 app.get("/", logger, (req, res) => {
   res.send(`Home Page`);
 });
