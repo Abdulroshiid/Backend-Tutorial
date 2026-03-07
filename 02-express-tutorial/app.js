643e123482038cf9affdb27515c88f3e738efe9e
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-07 12:31:10 +0100

    Continued  learning about middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,15 @@ const app = express();
 const PORT = 5000;
 
 // req => middleware => res
-app.get("/", (req, res) => {
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  res.send(`Testing middleware`);
+};
+app.get("/", logger, (req, res) => {
   res.send(`Home Page`);
 });
 app.get("/about", (req, res) => {
