5c6624c31910fe73ba30986797c357e3b1da7b74
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-07 12:11:34 +0100

    Started learning middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+// req => middleware => res
+app.get("/", (req, res) => {
+  res.send(`Home Page`);
+});
+app.get("/about", (req, res) => {
+  res.send(`About Page`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is Listening on port ${PORT}...`);
 });
