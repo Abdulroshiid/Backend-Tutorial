54949c35c9c6316ced1fc26a51a7280a4305372e
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-07 14:40:12 +0100

    Continued  learning about middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const logger = require("./logger");
+const data = require("./data");
 
 // req => middleware => res
+app.use(logger);
 
-app.get("/", logger, (req, res) => {
+app.get("/", (req, res) => {
   res.send(`Home Page`);
 });
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send(`About Page`);
+});
+app.get("/api/data", (req, res) => {
+  res.json(data.products[3]);
+});
+app.get("/api/data/people", (req, res) => {
+  res.json(data.people[0]);
 });
 
 app.listen(PORT, () => {
