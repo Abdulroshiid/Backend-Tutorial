8d31967383046d328df45800d2bd4f37de59ea32
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-10 22:47:40 +0100

    started learning the POST method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 let { people } = require("./data");
+// Static assets
+app.use(express.static("./methods-public"));
 
-console.log(people);
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}...`);
 });
