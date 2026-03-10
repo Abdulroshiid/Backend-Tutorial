a9d989cc6c3a47c8f82dcd103aefa19c0bc530d0
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-10 22:02:54 +0100

    About to start learning methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const morgan = require("morgan");
-const logger = require("./logger");
-const authorize = require("./authorize");
-const data = require("./data");
-
-// req => middleware => res
-// app.use([logger, authorize]);
-app.use(morgan("tiny"));
-app.get("/", (req, res) => {
-  res.send(`Home-Page`);
-});
-app.get("/about", (req, res) => {
-  res.send(`About-Page`);
-});
-app.get("/api/data", (req, res) => {
-  res.json(data.products);
-});
-app.get("/api/data/people", (req, res) => {
-  console.log(req.user);
-  res.json(data.people);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}...`);
