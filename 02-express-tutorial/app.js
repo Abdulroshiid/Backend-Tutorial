e15b7e7a9910a3eb64d82f28bdf57e0a5238e3d5
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-10 22:06:29 +0100

    About to start learning methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+let { people } = require("./data");
 
+console.log(people);
 app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}...`);
 });
