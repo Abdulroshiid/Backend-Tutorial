8513302b1289d01e31880aee931e79909ca51358
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-11 13:15:01 +0100

    continued learning the POST method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,9 +4,13 @@ const PORT = 5000;
 let { people } = require("./data");
 // Static assets
 app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+app.post("/login", (req, res) => {
+  res.send("POST");
 });
 
 app.listen(PORT, () => {
