b51115425678ebac5b603887e6292d1808924362
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-12 15:09:07 +0100

    continued learning the POST method

--- 02-express-tutorial/data.js
+++ 02-express-tutorial/data.js
@@ -36,7 +36,7 @@ const people = [
   { id: 1, name: "John" },
   { id: 2, name: "Peter" },
   { id: 3, name: "Susan" },
-  { id: 4, name: "Amma" },
+  { id: 4, name: "Anna" },
   { id: 5, name: "Emma" },
 ];
 module.exports = { products, people };
