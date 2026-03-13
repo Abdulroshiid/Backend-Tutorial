b463ecf5cd43fe59b695ae90a626f9032087173e
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-13 13:18:42 +0100

    continued learning the POST method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,13 +6,21 @@ let { people } = require("./data");
 app.use(express.static("./methods-public"));
 // Parse form data
 app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
 app.post("/api/people", (req, res) => {
-  res.status(201).send(`Successful`);
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: `Please, provide a user name.` });
+  }
+  res.status(201).send("success");
 });
 
 app.post("/login", (req, res) => {
