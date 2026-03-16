e827708502e1e78156dd3cb57c4a923c76e5384f
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-16 15:39:54 +0100

    Testing how postman works

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -61,7 +61,7 @@ app.put("/api/people/:id", (req, res) => {
     }
     return person;
   });
-  res.status(200).json({ newPeople });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(PORT, () => {
