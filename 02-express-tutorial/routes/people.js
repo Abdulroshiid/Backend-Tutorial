709280c825a0fbf203da76301886e489547822a7
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-16 17:05:59 +0100

    Getting to understand how the route works

---
+++ 02-express-tutorial/routes/people.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+// THE POST METHOD
+
+router.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `Please, provide a user name.` });
+  }
+  res.status(201).send({ succcess: true, person: name });
+});
+
+router.post("/api/people/postman", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `Please, providde a user name.` });
+  }
+  res.status(201).send({ succcess: true, data: [...people, name] });
+});
+
+// THE PUT METHOD
+
+router.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id} exists.` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+// THE DELETE METHOD
+
+router.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res.status(404).json({
+      succcess: false,
+      msg: `No person with id ${req.params.id} exists.`,
+    });
+  }
+  const newPerson = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  return res.status(200).json({ success: true, data: newPerson });
+});
