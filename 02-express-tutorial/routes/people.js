3b96f735741a0465b718a9b516704b9f6b5977bd
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-28 23:24:17 +0100

    Learning the controller setup

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// Fetching the components from the controllers
 const {
   getUser,
   post,
@@ -9,20 +10,14 @@ const {
   deleteUser,
 } = require("../controllers/people");
 
+//THE GET METHOD
 router.get("/", getUser);
-
 // THE POST METHOD
-
 router.post("/", post);
-
 router.post("/postuser", postUser);
-
 // THE PUT METHOD
-
 router.put("/:id", updateUser);
-
 // THE DELETE METHOD
-
 router.delete("/:id", deleteUser);
 
 module.exports = router;
