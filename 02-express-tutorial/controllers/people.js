98b77b950520808aea3ae28e073354256a0a4917
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-03-28 23:15:07 +0100

    Learning the controller setup

---
+++ 02-express-tutorial/controllers/people.js
@@ -0,0 +1,59 @@
+const getUser = (req, res) => {
+  res.status(200).json({ success: true, data: people });
+};
+
+const post = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `Please, provide a user name.` });
+  }
+  res.status(201).send({ succcess: true, person: name });
+};
+
+const postUser = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `Please, providde a user name.` });
+  }
+  res.status(201).send({ succcess: true, data: [...people, name] });
+};
+
+const putUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res.status(404).json({
+      success: false,
+      msg: `The person with id ${id} does not exists.`,
+    });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+};
+
+const deleteUser = (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res.status(404).json({
+      succcess: false,
+      msg: `No person with id ${req.params.id} exists.`,
+    });
+  }
+  const newPerson = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  return res.status(200).json({ success: true, data: newPerson });
+};
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -3,70 +3,20 @@ const router = express.Router();
 
 let { people } = require("../data");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+router.get("/");
 
 // THE POST METHOD
 
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: `Please, provide a user name.` });
-  }
-  res.status(201).send({ succcess: true, person: name });
-});
+router.post("/");
 
-router.post("/postman", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: `Please, providde a user name.` });
-  }
-  res.status(201).send({ succcess: true, data: [...people, name] });
-});
+router.post("/postuser");
 
 // THE PUT METHOD
 
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === Number(id));
-  if (!person) {
-    return res.status(404).json({
-      success: false,
-      msg: `The person with id ${id} does not exists.`,
-    });
-  }
-
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
+router.put("/:id");
 
 // THE DELETE METHOD
 
-router.delete("/:id", (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
-
-  if (!person) {
-    return res.status(404).json({
-      succcess: false,
-      msg: `No person with id ${req.params.id} exists.`,
-    });
-  }
-  const newPerson = people.filter(
-    (person) => person.id !== Number(req.params.id),
-  );
-  return res.status(200).json({ success: true, data: newPerson });
-});
+router.delete("/:id");
 
 module.exports = router;
